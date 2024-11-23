71854f426b991eb20144e17ff7d004cf1855114f
Boris zaidan <Bzmgs31@gmail.com>
2024-11-23 12:39:37 -0300

    Adicionei mais duas rotas forgotRresetpassword

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login/index";
 import Email from "./pages/SendEmail/index";
 import Cadastro from "./pages/Cadastro/index";
 import ListarUsuarios from "./pages/Lista/index";
+import Forgot from "./pages/Forgot";
+import ResetPassword from "./pages/ResetPassword";
 
 function AppRoutes() {
   return (
@@ -13,6 +15,8 @@ function AppRoutes() {
         <Route path="/Cadastro" element={<Cadastro />}></Route>
         <Route path="/SendEmail" element={<Email />}></Route>
         <Route path="/Lista" element={<ListarUsuarios />}></Route>
+        <Route path="/Forgot" element={<Forgot />}></Route>
+        <Route path="/ResetPassword" element={<ResetPassword />}></Route>
         <Route path="*" element={<Navigate to="/" replace />}></Route>
       </Routes>
     </div>
